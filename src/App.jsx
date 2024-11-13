433a31907761c1becf09b061a45d42bbddd33f55
Marcosskai <marcos.gabrielr150@gmail.com>
2024-11-13 14:39:47 -0300

    remove tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
 
 export function App() {
 
   return (
-    <div >
+    <div className="bg-white">
+
+      <Header/>
+
       <Post 
         author="Diego Fernandes"
         content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi, ut. Magnam incidunt labore porro! Sapiente molestiae perferendis sunt necessitatibus error architecto minima nemo aspernatur! Pariatur dolores nam deserunt veniam voluptate?"
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import "./Header.css"
+
+export function Header() {
+    return(
+        <div class="header">
+            <strong>Ignite Feed</strong>
+        </div>
+    )
+}
