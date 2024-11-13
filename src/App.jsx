42d1de56501bf0811e393cad78353f5f0399a332
Marcosskai <marcos.gabrielr150@gmail.com>
2024-11-13 13:56:29 -0300

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-function App() {
+import { Post } from "./Post"
+
+export function App() {
 
   return (
-    <>
-      <div>
-        <h1>Hello Word</h1>
-      </div>
-    </>
+    <div >
+      <Post 
+        author="Diego Fernandes"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi, ut. Magnam incidunt labore porro! Sapiente molestiae perferendis sunt necessitatibus error architecto minima nemo aspernatur! Pariatur dolores nam deserunt veniam voluptate?"
+      />
+      <Post 
+        author="Marcos Gabriel"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi, ut. Magnam incidunt labore porro! Sapiente molestiae perferendis sunt necessitatibus error architecto minima nemo aspernatur! Pariatur dolores nam deserunt veniam voluptate?"
+      />
+    </div>
   )
 }
-
-export default App
