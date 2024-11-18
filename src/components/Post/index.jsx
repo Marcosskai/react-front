68d6b78d11b74563d667cd3b23f2efb026eacacc
Marcosskai <marcos.gabrielr150@gmail.com>
2024-11-18 11:00:39 -0300

    commentsForm

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -34,7 +34,9 @@ export function Post() {
                 <textarea 
                     placeholder="Deixe um comentário"
                 />
-                <button type="submit">Comentário</button>
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
             </form>
         </article>
     )
